fb9cf93013e088757d732bad3a028228f677f04e
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-01 13:23:10 -0300

    feat: added authentication urls

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.blog.urls')),
+    path('api/', include('apps.authentication.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
