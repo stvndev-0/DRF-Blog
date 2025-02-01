459c5d2f8dc86bd0df221bb1222ad55a44cea010
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-01 13:29:51 -0300

    feat: configuration of celery base, addition of tasks and periodic tasks

---
+++ core/celery.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+app = Celery('core')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'eliminar-usuarios-no-verificados': {
+        'task': 'apps.authentication.tasks.account_not_verify',
+        'schedule': 60.0,  # Cada 10 minutos
+    },
+}
