97fd6918b5bb9b5b57ce51566bda2cdd292b7362
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-01 19:58:47 -0300

    feat: added urls for the authentication

---
+++ apps/authentication/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    ResetPasswordView,
+    SendResetEmailView,
+    ActivationEmailView,
+    SignUpView,
+    LoginView)
+
+urlpatterns = [
+    path('reset-password/<str:uidb64>/<str:token>', ResetPasswordView.as_view(), name='password-reset-confirm'),
+    path('send-reset-email', SendResetEmailView.as_view()),
+    path('activation-email/<str:uidb64>/<str:token>', ActivationEmailView.as_view(), name='activation-email'),
+    path('signup', SignUpView.as_view()),
+    path('login', LoginView.as_view())
+]
