7357409d59545c2edfdbfab1cdf9d69f93f8d510
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-07 18:42:59 -0300

    feat: added JWT authentication setup

--- core/settings.py
+++ core/settings.py
@@ -150,3 +150,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
