a3597a073d4c05b899a389ed701a3cea114bf053
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-07 18:50:31 -0300

    refactor: remove unused Login endpoint

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -3,13 +3,11 @@ from .views import (
     ResetPasswordView,
     SendResetEmailView,
     ActivationEmailView,
-    SignUpView,
-    LoginView)
+    SignUpView)
 
 urlpatterns = [
     path('reset-password/<str:uidb64>/<str:token>', ResetPasswordView.as_view(), name='password-reset-confirm'),
     path('send-reset-email', SendResetEmailView.as_view()),
     path('activation-email/<str:uidb64>/<str:token>', ActivationEmailView.as_view(), name='activation-email'),
-    path('signup', SignUpView.as_view()),
-    path('login', LoginView.as_view())
+    path('signup', SignUpView.as_view())
 ]
