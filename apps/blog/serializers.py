98d920abf7d290bc2305f2141f087e0fd67546cb
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-17 01:03:18 -0300

    feat: PostSerializer to create posts

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Post
+from .models import Category, Post, Comment
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,4 +15,17 @@ class PostListSerializer(serializers.ModelSerializer):
             'slug',
             'updated_at',
             'user',
-        ] 
+        ]
+
+class PostSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=155, required=True)
+    description = serializers.CharField(max_length=255)
+    content = serializers.CharField(required=True)
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        required=True)
+    
+    class Meta:
+        model = Post
+        fields = '__all__'
+        read_only_fields = ('user',)
