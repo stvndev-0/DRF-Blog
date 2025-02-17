759ea1957eb7b6b4482aa6d53f548687e1f8a22c
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-17 01:04:19 -0300

    feat URL router for PostCreateView

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CategoryListView, PostByCategoryView
+from .views import (
+    CategoryListView, PostByCategoryView,
+    PostCreateView)
 
 urlpatterns = [
     path('category', CategoryListView.as_view(), name='categories'),
-    path('category/<slug:slug>', PostByCategoryView.as_view(), name='posts-by-category')
+    path('category/<slug:slug>', PostByCategoryView.as_view(), name='posts-by-category'),
+    path('posts', PostCreateView.as_view(), name='posts-create'),
 ]
