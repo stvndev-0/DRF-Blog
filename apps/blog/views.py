aadc137a7e3d2d4563d502ccd71d7fd9354edeeb
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-17 00:31:28 -0300

    feat: PostByCategoryView to get all the posts of the category

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Post
+from .serializers import CategorySerializer, PostListSerializer
 
 # Category
 class CategoryListView(generics.ListAPIView):
@@ -10,3 +11,13 @@ class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+class PostByCategoryView(generics.ListAPIView):
+    """
+    Obtiene los posts de una categoria.
+    """
+    serializer_class = PostListSerializer
+
+    def get_queryset(self):
+        slug = self.kwargs.get('slug', None)
+        category = get_object_or_404(Category, slug=slug)
+        return Post.objects.filter(category=category, status='Public')
