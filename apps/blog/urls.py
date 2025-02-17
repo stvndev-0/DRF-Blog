18134ceb815f7a0349c2c061e45a6fb0a0653995
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-17 18:27:58 -0300

    feat: URLs router for PostDetailUpdateDestroyView, CommentCreateView and CommentUpdateDestroyView

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import (
     CategoryListView, PostByCategoryView,
-    PostCreateView)
+    PostCreateView, PostDetailUpdateDestroyView,
+    CommentCreateView, CommentUpdateDestroyView)
 
 urlpatterns = [
     path('category', CategoryListView.as_view(), name='categories'),
     path('category/<slug:slug>', PostByCategoryView.as_view(), name='posts-by-category'),
     path('posts', PostCreateView.as_view(), name='posts-create'),
+    path('posts/<slug:slug>', PostDetailUpdateDestroyView.as_view(), name='posts-detail-update-delete'),
+    path('posts/<slug:slug>/comments', CommentCreateView.as_view(), name='comment-create'),
+    path('comments/<int:pk>', CommentUpdateDestroyView.as_view(), name='comment-update-delete')
 ]
