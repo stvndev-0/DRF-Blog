dae5a7b22b507eb7962f36c2c140fc98fa5bb303
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-17 00:33:47 -0300

    feat: PostListSerializer to return a list of posts

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Post
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+class PostListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'description',
+            'slug',
+            'updated_at',
+            'user',
+        ] 
