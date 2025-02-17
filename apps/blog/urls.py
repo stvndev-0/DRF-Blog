4ff4e383121e30bee4d205f84ad044628159bac6
stvndev-0 <esteban.villasecatt@gmail.com>
2025-02-17 00:34:24 -0300

    feat: URL route for PostByCategoryView

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CategoryListView
+from .views import CategoryListView, PostByCategoryView
 
 urlpatterns = [
-    path('category', CategoryListView.as_view(), name='categories')
+    path('category', CategoryListView.as_view(), name='categories'),
+    path('category/<slug:slug>', PostByCategoryView.as_view(), name='posts-by-category')
 ]
