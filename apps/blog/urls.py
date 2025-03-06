1f1645056f2bad1c1a2700eea6d2cd5daecfc0ee
stvndev-0 <esteban.villasecatt@gmail.com>
2025-03-06 17:43:39 -0300

    feat: add new URLs for likes

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     CategoryListView, PostByCategoryView,
     PostCreateView, PostDetailUpdateDestroyView,
-    CommentCreateView, CommentUpdateDestroyView)
+    CommentCreateView, CommentUpdateDestroyView,
+    PostLikesCreateView, PostLikesDestroyView)
 
 urlpatterns = [
     path('category', CategoryListView.as_view(), name='categories'),
@@ -10,5 +11,7 @@ urlpatterns = [
     path('posts', PostCreateView.as_view(), name='posts-create'),
     path('posts/<slug:slug>', PostDetailUpdateDestroyView.as_view(), name='posts-detail-update-delete'),
     path('posts/<slug:slug>/comments', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>', CommentUpdateDestroyView.as_view(), name='comment-update-delete')
+    path('comments/<int:pk>', CommentUpdateDestroyView.as_view(), name='comment-update-delete'),
+    path('posts/<slug:slug>/likes', PostLikesCreateView.as_view(), name='post-likes'),
+    path('likes/<int:pk>', PostLikesDestroyView.as_view(), name='likes-delete'),
 ]
